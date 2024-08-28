722cecddc49be1f9be65ac454fee729fb91f383f
Peter Marshall <petms@proton.me>
2024-08-28 01:09:14 -0400

    Add CA certificates to the image

--- pkgs/docker-image.nix
+++ pkgs/docker-image.nix
@@ -5,7 +5,7 @@ dockerTools.buildLayeredImage {
   name = "contact-api";
   tag = "latest";
 
-  contents = [ contact-api ];
+  contents = [ contact-api pkgs.cacert ];
 
   config = {
     Cmd = [ "${contact-api}/bin/contact-api" ];
